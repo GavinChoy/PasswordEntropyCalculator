995b64677f0e0044173e5f66c9ec6d02aa4f58b8
Gavin Choy <daetsugavin@gmail.com>
2021-02-03 21:32:19 +0000

    Added more code documentation. Added GNU v3 licensing details. Added README contents.

--- EntropyCalc.cc
+++ EntropyCalc.cc
@@ -1,3 +1,21 @@
+/**
+* Password Entropy Calculator
+* Copyright (C) 2021  Gavin Choy
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include <cmath>
 #include "calc-helper.cc"
@@ -6,12 +24,20 @@ using namespace std;
 double calculate_entropy(bool lower_alpha, bool upper_alpha, bool numbers, bool special, int length);
 
 int main(){
+
+    cout << "Password Entropy Calculator  Copyright (C) 2021  Gavin Choy\n";
+    cout << "This program comes with ABSOLUTELY NO WARRANTY.\n";
+    cout << "This is free software, and you are welcome to redistribute it\n";
+    cout << "under certain conditions.\n\n";
+
+    // Properties of the password
     bool lower_alpha = false;
     bool upper_alpha = false;
     bool numbers = false;
     bool special = false;
     int length = 0;
 
+    // Prepares the while loops
     string response = "default";
 
     while ((response != "y") && (response != "n")){
--- calc-helper.cc
+++ calc-helper.cc
@@ -1,3 +1,20 @@
+/**
+* Password Entropy Calculator
+* Copyright (C) 2021  Gavin Choy
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 double calculate_entropy(bool lower_alpha, bool upper_alpha, bool numbers, bool special, int length){
     /**
